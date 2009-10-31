f4a5fd183c3e4deabec8d30ee81730e572c662f9
Paul Rogers <paul@dingosky.com>
2009-10-30 21:51:07 -0700

    Minor mod to basic resource example.

--- examples/basic_resource.rb
+++ examples/basic_resource.rb
@@ -6,29 +6,27 @@ PersonData.keygen :name
 
 class Person
   include JiakResource
-
-  server       'http://localhost:8002/jiak'
-  group        'people'
-  data_class   PersonData
-  auto_post    true
+  server      'http://localhost:8002/jiak'
+  group       'people'
+  data_class  PersonData
+  auto_post   true
+  auto_update true
 end
 
-remy = Person.new(:name => 'remy', :age => 10)
-puts Person.get('remy').name                # => "remy"
+remy = Person.new(:name => 'remy',:age => 10) #            (auto-post)
+puts remy.name                                # => "remy"  (auto-update)
 
-remy.name = "Remy"
-remy.update
-puts remy.name                              # => "Remy"
-puts Person.get('remy').name                # => "Remy"
+puts Person.get('remy').name                  # => "remy"  (from Jiak server)
+puts Person.get('remy').age                   # => 10      (from Jiak server)
 
-callie = Person.new(:name => 'Callie', :age => 12)
-remy.bi_link(callie,'sister').update
-callie.update
+remy.age = 11                                 #            (auto-update)
+puts Person.get('remy').age                   # => 11      (from Jiak server)
+
+callie = Person.new(:name => 'Callie', :age => 13)
+remy.link(callie,'sister')
 
 sisters = remy.query(Person,'sister')
-puts sisters[0].eql?(callie)                # => true
+puts sisters[0].eql?(callie)                  # => true
 
 remy.delete
 callie.delete
-
-
--- lib/riakrest.rb
+++ lib/riakrest.rb
@@ -99,35 +99,38 @@ $:.unshift(File.dirname(__FILE__)) unless
 # create resource objects that encapsulate a lot of the cruft of core client
 # interaction. We'll do the same steps as above using resources.
 # <code>
-#   require 'riakrest'
-#   include RiakRest
-#   
-#   PersonData = JiakDataHash.create(:name,:age)
-#   PersonData.keygen :name
-#   
-#   class Person
-#     include JiakResource
-#     server      'http://localhost:8002/jiak'
-#     group       'people'
-#     data_class  PersonData
-#     auto_post   true
-#     auto_update true
-#   end
-#   
-#   remy = Person.new(:name => 'remy', :age => 10)
-#   puts Person.get('remy').name                # => "remy"
-#   
-#   remy.name = "Remy"
-#   puts Person.get('remy').name                # => "Remy"
-#   
-#   callie = Person.new(:name => 'Callie', :age => 12)
-#   remy.link(callie,'sister')
-#   
-#   sisters = remy.query(Person,'sister')
-#   sisters[0].eql?(callie)                     # => true
-#   
-#   remy.delete
-#   callie.delete
+#  require 'riakrest'
+#  include RiakRest
+# 
+#  PersonData = JiakDataHash.create(:name,:age)
+#  PersonData.keygen :name
+# 
+#  class Person
+#    include JiakResource
+#    server      'http://localhost:8002/jiak'
+#    group       'people'
+#    data_class  PersonData
+#    auto_post   true
+#    auto_update true
+#  end
+# 
+#  remy = Person.new(:name => 'remy',:age => 10) #            (auto-post)
+#  puts remy.name                                # => "remy"  (auto-update)
+# 
+#  puts Person.get('remy').name                  # => "remy"  (from Jiak server)
+#  puts Person.get('remy').age                   # => 10      (from Jiak server)
+#
+#  remy.age = 11                                 #            (auto-update)
+#  puts Person.get('remy').age                   # => 11      (from Jiak server)
+#
+#  callie = Person.new(:name => 'Callie', :age => 13)
+#  remy.link(callie,'sister')
+#
+#  sisters = remy.query(Person,'sister')
+#  puts sisters[0].eql?(callie)                  # => true
+#
+#  remy.delete
+#  callie.delete
 # </code>
 # Ah, that feels better. Go forth and Riak!
 module RiakRest
